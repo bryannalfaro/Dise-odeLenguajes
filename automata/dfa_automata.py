f5ae8d326833c2f3657d3857d01df1b8cac8271b
Bryann Alfaro <alf19372@uvg.edu.gt>
2023-03-04 17:01:28 -0600

    cleaning prints

--- automata/dfa_automata.py
+++ automata/dfa_automata.py
@@ -34,20 +34,16 @@ class DFA_automata(Automata):
     def make_pi_new(self,partition2):
         temporal = {}
         new_arr = []
-        print('ALFAAA',self.alphabet)
-        #print('partition2',partition2)
-        for i in range(0, len(partition2)):
-            print('1i',i,partition2[i])
+
+        for i in range(0, len(partition2)): #recorro los grupos de la particion
             if len(partition2[i])>1: #No hay solo un estado en el grupo
-                for state in partition2[i]:
-                    #print('sSSState',state)
+                for state in partition2[i]: #recorro los estados del grupo
                     #vaciar temporal cuando se cambia de simbolo
                     for symbol in self.alphabet:
                         if symbol!=Symbol('#').name or symbol!=Symbol('ε').name:
-                            #If the move of a state is in the same group
-                            #print('symbol AA',symbol)
+                           #Hacer movimiento para verificar el grupo al que se mueve
                             move_state = self.move(state,symbol)
-                            #print('move_state',move_state)
+                            #Si no tiene transicion se agrega un estado muerto
                             if move_state==None:
                                 #print('HERE NONEEE',state)
                                 if state in temporal:
@@ -55,7 +51,7 @@ class DFA_automata(Automata):
                                 else:
                                     temporal[state] = {symbol:[1000]}
                             else:
-                                for iu in range(0, len(partition2)):
+                                for iu in range(0, len(partition2)): #Se verifica a que grupo va
                                     #print('HEE',move_state,partition2[i],symbol)
                                     #print('PARTITION2',partition2[iu])
                                     if move_state in partition2[iu]:
@@ -69,18 +65,19 @@ class DFA_automata(Automata):
                                             break
 
                                     #print('temporal2',temporal)
-                print('temporal',temporal)
+                #print('temporal',temporal)
                 #print('ajaj',i)
                 flipped = {}
                 ignored = []
+                #Por cada simbolo se verifica si hay un cambio de grupo, se arma un diccionario
+                #de la forma grupo: [estados] para [estados] sean un nuevo grupo
                 for symbol in range(0, len(self.alphabet)):
                     if self.alphabet[symbol]!=Symbol('#').name or self.alphabet[symbol]!=Symbol('ε').name:
-                        #agrupar por esa letra
                         flipped = {}
                         for key, value in temporal.items():
                             if key not in ignored:
                                 if self.alphabet[symbol] in value:
-                                    print('je',value,value[self.alphabet[symbol]])
+                                    #print('je',value,value[self.alphabet[symbol]])
                                     if tuple(value[self.alphabet[symbol]]) not in flipped and tuple(value[self.alphabet[symbol]])!=[]:
                                         if len(flipped)==0:
                                             flipped[tuple(value[self.alphabet[symbol]])] = [key]
@@ -93,35 +90,22 @@ class DFA_automata(Automata):
                                         flipped[tuple(value[self.alphabet[symbol]])].append(key)
 
                                 else:
-                                    print('KEYEYYYY',key)
+                                    #print('KEYEYYYY',key)
                                     ignored.append(key)
-                        print("internal",flipped,ignored)
+                        #print("internal",flipped,ignored)
                         if ignored!=[]:
-                            print('breaak')
+                            #print('breaak')
                             break
-                print('YEEE')
-                print('Y',partition2)
-                #partition with ignored states
-                #print('i',i)
-                #print('HEEEEJFALKS',partition2[i],state)
-
 
                 for key,value in flipped.items():
                     new_arr.append(value)
 
-
-                print('partition2 FLIPPEDD',partition2)
-                '''print('YAAA')
-                for state in ignored:
-                    partition2[i].remove(state)
-                    partition2.append([state])
-                #print('partition2 YAAA',partition2)'''
-
                 temporal = {}
             else:
+                #Si solo es un estado, se apendea al new_arr
                 new_arr.append(partition2[i])
 
-        print('NEWWW',new_arr)
+        #print('NEWWW',new_arr)
         #delete empty groups
         for i in range(0, len(new_arr)):
             if len(new_arr[i])==0:
@@ -145,7 +129,7 @@ class DFA_automata(Automata):
         new_partition = copy.copy(partition)
         while True:
                 new_partition2 = self.make_pi_new(new_partition)
-                print('new_partition2 RESULTAT',new_partition2)
+                #print('new_partition2 RESULTAT',new_partition2)
                 if new_partition2 == partition:
                      break
                 else:
